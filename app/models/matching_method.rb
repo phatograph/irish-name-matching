e2a52218d8be668dd59891ccf7492bcefa8f954d
Phat Wangrungarun <phatograph@gmail.com>
2015-04-02 15:02:25 +0100

    Make private

--- app/models/matching_method.rb
+++ app/models/matching_method.rb
@@ -7,12 +7,14 @@ class MatchingMethod
   def initialize(params = {})
     @name      = params.fetch(:name)
     @base_name = params.fetch(:base_name)
-    cal_score()
+    cal_score
   end
 
   def get_score
     @score.round(3)
   end
+
+  private
 
   def cal_score
     raise NotImplementedError
@@ -20,6 +22,8 @@ class MatchingMethod
 end
 
 class LD < MatchingMethod
+  private
+
   def cal_score
     @value = Text::Levenshtein.distance(@name, @base_name.name)
     size   = [@name.size, @base_name.name.size].max
@@ -28,6 +32,8 @@ class LD < MatchingMethod
 end
 
 class Soundex < MatchingMethod
+  private
+
   def cal_score
     @value           = Text::Soundex.soundex(@name)
     soundex_distance = Text::Levenshtein.distance(@value, @base_name.soundex)
