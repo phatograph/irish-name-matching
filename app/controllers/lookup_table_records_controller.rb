02969c1b1abd29ddd449db8ca7826555e302ce3b
Phat Wangrungarun <phatograph@gmail.com>
2015-04-22 20:23:46 +0100

    Lookup table interface

---
+++ app/controllers/lookup_table_records_controller.rb
@@ -0,0 +1,34 @@
+class LookupTableRecordsController < ApplicationController
+  def index
+    @records = LookupTableRecord.all.order(:name)
+  end
+
+  def new
+    @record = LookupTableRecord.new
+  end
+
+  def create
+    @record = LookupTableRecord.new(permitted_params)
+
+    if @record.save
+      redirect_to lookup_table_records_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    record = LookupTableRecord.find(params[:id])
+    record.destroy
+    redirect_to :back
+  end
+
+  private
+
+    def permitted_params
+      params.require(:lookup_table_record).permit(
+        :name,
+        :ref
+      )
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lookup_table_records
   post '/match' => 'home#match'
   root 'home#index'
 end
